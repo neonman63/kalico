8625f7f24852e80dd09b49d747836c5808942ff6
Rogerio Goncalves <rogerlz@gmail.com>
2024-12-10 22:06:12 +0000

    homing: allow homing_accel to be configurable

--- klippy/stepper.py
+++ klippy/stepper.py
@@ -461,6 +461,10 @@ class PrinterRail:
             "min_home_dist", self.homing_retract_dist, minval=0.0
         )
 
+        self.homing_accel = config.get("homing_accel", None)
+        if self.homing_accel is not None:
+            self.homing_accel = config.getfloat("homing_accel", minval=0.0)
+
         if self.homing_positive_dir is None:
             axis_len = self.position_max - self.position_min
             if self.position_endstop <= self.position_min + axis_len / 4.0:
@@ -507,6 +511,7 @@ class PrinterRail:
                 "second_homing_speed",
                 "use_sensorless_homing",
                 "min_home_dist",
+                "accel",
             ],
         )(
             self.homing_speed,
@@ -517,6 +522,7 @@ class PrinterRail:
             self.second_homing_speed,
             self.use_sensorless_homing,
             self.min_home_dist,
+            self.homing_accel,
         )
         return homing_info
 
